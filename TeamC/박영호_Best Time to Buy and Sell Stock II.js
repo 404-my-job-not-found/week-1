4dba673451913eeb5e4a7386be1f6c42c2b9c22e
hexdrinker <hexdrinker2020@gmail.com>
2025-06-25 22:24:19 +0900

    solve: Best Time to Buy and Sell Stock II

---
+++ TeamC/박영호_Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,13 @@
+var maxProfit = function (prices) {
+  let max = 0;
+
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (prices[i] >= prices[i + 1]) {
+      continue;
+    } else {
+      max += prices[i + 1] - prices[i];
+    }
+  }
+
+  return max;
+};
