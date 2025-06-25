468675f7a8a58e16fc951fcb583bd6b3b3cf9ece
hexdrinker <hexdrinker2020@gmail.com>
2025-06-25 20:55:54 +0900

    solve: Remove Duplicates from Sorted Array

---
+++ TeamC/박영호_Remove Duplicates from Sorted Array.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function (nums) {
+  if (nums.length <= 0) return 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i -= 1;
+    }
+  }
+
+  return nums.length;
+};
